1add999c52f0849201d1996bf10c37c5774c9fa5
Mostafa Khodeir <mostafa.khodeir@zalando.de>
2019-01-24 18:37:52 +0100

    Adding Progress and SHow error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { JsonConvererServiceService } from './services/json-converer-service.service';
 
 @Component({
@@ -8,8 +8,19 @@ import { JsonConvererServiceService } from './services/json-converer-service.ser
 })
 export class AppComponent {
 
+  @ViewChild('editor') editor;
+  options: any = {
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,
+  };
 
   constructor(private jsonConvererServiceService: JsonConvererServiceService) {
+  }
+
+  ngOnInit() {
+    // disable Automatically scrolling cursor into view after selection change warning
+    this.editor.getEditor().$blockScrolling = Infinity;
   }
 
   json: string; //Output
@@ -21,7 +32,18 @@ export class AppComponent {
     this.jsonConvererServiceService.convertToJson(this.edn).subscribe(
       jsonResult => {
         this.json = jsonResult;
+      }, error => {
+        alert("Failed to Parse");
       }
     );
   }
+
+  beautify() {
+
+    // var beautify = ace.require("ace/ext/beautify"); // get reference to extension
+    // var editor = ace.edit("editor"); // get reference to editor
+    // beautify.beautify(editor.session);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
@@ -15,6 +15,7 @@ import { AceEditorModule } from 'ng2-ace-editor';
     HttpClientModule,
     NgxJsonViewerModule,
     AceEditorModule,
+    LoadingBarHttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
